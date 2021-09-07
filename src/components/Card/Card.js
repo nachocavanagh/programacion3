8c7082d78ddf494eec5b88d22737d537be67d579
nachocavanagh <icavanaghnicholson@udesa.edu.ar>
2021-09-07 20:11:06 -0300

    ver mas

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,12 +1,28 @@
 import React, {Component} from "react"
+import './card.css'
 
 class Card extends Component {
 constructor(props){
     super (props);
     this.state = {
-        Albumes:[]
-
+        Albumes:[],
+        viewMore: false,
+        text: 'Ver más',
     }
+}
+
+viewMore(){
+  if(this.state.viewMore){
+      this.setState({
+          viewMore: false,
+          text: 'Ver más'
+      })
+  } else {
+      this.setState({
+          viewMore: true,
+          text: 'Ver menos'
+      })            
+  }
 }
 
 render () {
@@ -20,13 +36,19 @@ render () {
                     <i className="far fa-window-close"></i>
                   </section>
                   <main>
-                    <img src={this.props.dataAlbum.cover} alt="" />
+                    <div className="image-container">
+                      <img className="image" src={this.props.dataAlbum.cover_big} alt="" />
+                    </div>
                       <h3>{this.props.dataAlbum.title}</h3>
-                      <p className="description"></p>
-                      <section className="aditional-info">
-                        
+                      <p className="description">{this.props.dataAlbum.record_type.toUpperCase()}</p>
+                      <section className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>
+                        <p>Artist:</p>
+                        <div className="artist-data">
+                          <img className="artist-img" src={this.props.dataAlbum.artist.picture_small} alt="" />
+                          <p>{this.props.dataAlbum.artist.name}</p>
+                        </div>
                       </section>
-                      <a href="">Ver más</a>
+                      <p className='view-more' onClick={()=>this.viewMore()}>{this.state.text}</p>
                   </main>
                 </article>
             
